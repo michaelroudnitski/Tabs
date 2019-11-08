24f61b6b423a8e661a8d997f7d6f6849dd7f6db7
Michael <michaelroudnitski@gmail.com>
2019-11-08 16:31:44 -0500

    loading spinner and no open tabs

--- lib/tabsList.dart
+++ lib/tabsList.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tabs/widgets/tabCard.dart';
 import 'package:flutter_money_formatter/flutter_money_formatter.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class TabsList extends StatelessWidget {
   String getTotalAmountFormatted(QuerySnapshot tabsData) {
@@ -17,7 +18,14 @@ class TabsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<QuerySnapshot>(
       builder: (context, tabsData, child) {
-        if (tabsData != null)
+        if (tabsData == null)
+          return Center(
+            child: SpinKitDoubleBounce(
+              color: Theme.of(context).primaryColor,
+              size: 50.0,
+            ),
+          );
+        else if (tabsData.documents.length > 100)
           return Column(
             children: <Widget>[
               Container(
@@ -56,7 +64,36 @@ class TabsList extends StatelessWidget {
             ],
           );
         else
-          return Text("No open tabs");
+          return Column(
+            children: <Widget>[
+              Container(
+                height: 120,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Text(
+                      "${tabsData.documents.length} OPEN TABS",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500, color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(getTotalAmountFormatted(tabsData),
+                        style: Theme.of(context).textTheme.display2)
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Center(
+                child: Text("NO OPEN TABS"),
+              ),
+            ],
+          );
       },
     );
   }
