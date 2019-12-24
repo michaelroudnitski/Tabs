db6f10d96ac8fd0d05324a285496e6926c136176
Michael <michaelroudnitski@gmail.com>
2019-12-23 22:08:47 -0500

    update names suggestions

--- lib/providers/suggestionsState.dart
+++ lib/providers/suggestionsState.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:tabs/controllers/suggestionsController.dart';
 
 class Suggestions extends ChangeNotifier {
+  static const int _NAMES_LEN = 3;
+  static const int _AMOUNTS_LEN = 5;
+  static const int _DESCRIPTIONS_LEN = 3;
+  /* defaults */
   Map<String, List<String>> suggestions = {
     "names": [],
     "amounts": ["5", "10", "20", "50"],
@@ -10,14 +14,33 @@ class Suggestions extends ChangeNotifier {
   };
 
   Suggestions() {
-    updateFromDatabase();
+    _fetchFromDatabase();
   }
 
-  void updateFromDatabase() async {
+  void _fetchFromDatabase() async {
     Map<String, dynamic> suggestions =
         await SuggestionsController.fetchSuggestions();
-    print(suggestions);
     if (suggestions != null) this.suggestions = suggestions;
     notifyListeners();
+  }
+
+  void updateSuggestions(String name, String amount, String description) {
+    _updateNames(name);
+    // TODO: amounts & descriptions
+    notifyListeners();
+    SuggestionsController.updateSuggestions(suggestions);
+  }
+
+  void _updateNames(String name) {
+    /* Use queues as LRU cache */
+    DoubleLinkedQueue<String> names = Queue.of(suggestions["names"]);
+    if (names.contains(name)) {
+      names.remove((a) => a == name);
+      names.addFirst(name);
+    } else {
+      if (names.length >= _NAMES_LEN) names.removeLast();
+      names.addFirst(name);
+    }
+    suggestions["names"] = List.of(names);
   }
 }
